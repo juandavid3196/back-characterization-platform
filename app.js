e735f64da66ff0072c6c2007fe8de3152d1016a5
juan david rios <juandavid3196@gmail.com>
2023-11-05 16:14:31 -0500

    subEventState and EventState Routes files

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ app.use(express.json());
 //programming API
 const eventRoutes = require('./routes/eventRoutes');
 const subeventRoutes = require('./routes/subeventRoutes');
+const evenStateRoutes = require('./routes/eventStateRoutes');
+const subEvenStateRoutes = require('./routes/subEventStateRoutes');
 
 app.use('/subevent', subeventRoutes);
 app.use('/event', eventRoutes);
+app.use('/eventstate', evenStateRoutes);
+app.use('/subeventstate', subEvenStateRoutes);
 
 
 app.get('/', (req, res) => {
---
+++ routes/eventStateRoutes.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../db');
+
+// Ruta para obtener todos los elementos
+router.get('/', (req, res) => {
+    db.query('SELECT * FROM eventstates', (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return;
+        }
+        return res.status(200).json(results);
+    });
+});
+
+
+// Ruta para obtener un elemento único por ID
+router.get('/:id', (req, res) => {
+    const itemId = req.params.id;
+
+    db.query('SELECT * FROM eventstates WHERE id = ?', [itemId], (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+        } else {
+            console.log('Resultados de la consulta:', results);
+            if (results.length === 0) {
+                res.status(404).json({ error: 'Evento no encontrado' });
+            } else {
+                res.status(200).json(results[0]);
+            }
+        }
+    });
+});
+
+
+// Ruta para crear un nuevo elemento
+router.post('/', (req, res) => {
+    const newItem = req.body;
+
+    // Realiza la inserción de datos en la base de datos
+    db.query('INSERT INTO eventstates SET ?', newItem, (error, results, fields) => {
+        if (error) {
+            console.error('Error al insertar los datos:', error);
+            res.status(500).json({ error: 'Error al insertar los datos en la base de datos' });
+        } else {
+            console.log('Datos insertados con éxito.');
+            res.status(201).json({ message: 'Datos insertados con éxito' });
+        }
+    });
+});
+
+// Ruta para actualizar un elemento por ID
+router.put('/:id', (req, res) => {
+    const itemId = req.params.id;
+    const updatedItem = req.body;
+
+    // Realiza la actualización de datos en la base de datos
+    db.query('UPDATE eventstates SET ? WHERE id = ?', [updatedItem, itemId], (error, results, fields) => {
+        if (error) {
+            console.error('Error al actualizar los datos:', error);
+            res.status(500).json({ error: 'Error al actualizar los datos en la base de datos' });
+        } else {
+            console.log('Datos actualizados con éxito.');
+            res.status(200).json({ message: 'Datos actualizados con éxito' });
+        }
+    });
+});
+
+
+// Ruta para eliminar un elemento por ID
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    // Realiza la eliminación del recurso en la base de datos
+    db.query('DELETE FROM eventstates WHERE id = ?', [id], (error, results, fields) => {
+        if (error) {
+            console.error('Error al eliminar el recurso:', error);
+            res.status(500).json({ error: 'Error al eliminar el recurso en la base de datos' });
+        } else {
+            console.log('Recurso eliminado con éxito.');
+            res.status(200).json({ message: 'Recurso eliminado con éxito' });
+        }
+    });
+});
+
+
+module.exports = router;
